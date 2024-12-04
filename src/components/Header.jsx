45c16f7b91f85001d6f1c4e21f9a5b7ecdef0096
fisayo-dev <olufisayobadina@gmail.com>
2024-12-04 13:56:42 +0100

    creating header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,34 @@
+import {
+  GlobeEuropeAfricaIcon,
+  ShoppingBagIcon,
+} from "@heroicons/react/24/outline";
 import { ShoppingCartIcon } from "@heroicons/react/24/outline";
+import { SearchNormal, User } from "iconsax-react";
 const Header = () => {
   return (
-    <div className="flex">
-      <ShoppingCartIcon className="w-6 h-6" />
-      <h2 className="text-2xl">Splaze</h2>
+    <div className="sticky top-0">
+      <div className="py-5">
+        <div className="container flex items-center justify-between gap-16 md:px-20 px-5">
+          <div className="flex text-green-800 items-center gap-2">
+            <ShoppingCartIcon className="w-7 h-7" />
+            <h2 className="text-2xl font-bold">Splaze</h2>
+          </div>
+          <div className="flex px-5 py-3 space-x-4 border-2 w-full rounded-2xl">
+                      <SearchNormal />
+                      <input type="text" placeholder="Search for products, brand and categories" className="w-full"/>
+          </div>
+          <div className="flex items-center space-x-7">
+            <div className="flex items-center gap-2">
+              <GlobeEuropeAfricaIcon className="w-6 h-6" />
+              <p className="">ENG</p>
+            </div>
+            <User className="w-6 h-6 " />
+            <div className="rounded-full cursor-pointer hover:bg-green-600 bg-green-400 p-3">
+              <ShoppingCartIcon className="h-6 w-6 text-white"/>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
