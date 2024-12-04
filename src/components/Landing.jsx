88b969d1e3499c4cfd595fa0377542d98b4f0f35
fisayo-dev <olufisayobadina@gmail.com>
2024-12-04 22:00:11 +0100

    created right part of landing page

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,25 +1,31 @@
+import landing_social_media from "../assets/app_images/landing_social_media.png";
 const Landing = () => {
   return (
     <div>
       <div className="py-5">
         <div className="container mx-auto md:px-16 px-5">
-          <div className="mt-5">
-            <div className="flex gap-10 items-center">
-              <div className="grid gap-4 text-left">
-                <h2 className="text-4xl capitalize font-bold">
-                  Your All-in-one online store.
-                </h2>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim
-                  sint aspernatur quis, inventore doloribus recusandae mollitia
-                  aliquam cumque voluptas fugiat?
-                </p>
-                <div>
-                  <button className="px-5 py-3 rounded-full bg-yellow-400">
-                    Start purchasing
-                  </button>
-                </div>
+          <div className="grid md:flex gap-5 justify-between items-center">
+            <div className="grid gap-4 text-left">
+              <h2 className="text-6xl capitalize font-bold">
+                Your All-in-one online store.
+              </h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim
+                sint aspernatur quis, inventore doloribus recusandae mollitia
+                aliquam cumque voluptas fugiat?
+              </p>
+              <div>
+                <button className="px-5 py-3 rounded-full bg-yellow-400">
+                  Start purchasing
+                </button>
               </div>
+            </div>
+            <div className="grid items-center">
+              <img
+                src={landing_social_media}
+                alt=""
+                className="ml-auto w-5/6"
+              />
             </div>
           </div>
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import { Header, Landing } from "../components";
 
 const Home = () => {
-    return <>
-        <Header />
-        <Landing />
-  </>
+  return (
+    <>
+      <Header />
+      <Landing />
+    </>
+  );
 };
 
 export default Home;
