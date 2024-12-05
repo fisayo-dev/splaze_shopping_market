e722e7932a8396bb18c660e50c8970544aa4e867
fisayo-dev <olufisayobadina@gmail.com>
2024-12-05 20:52:47 +0100

    changed price for dummy products

--- src/components/TrendingProducts.jsx
+++ src/components/TrendingProducts.jsx
@@ -8,10 +8,10 @@ const TrendingProducts = () => {
           <h2 className="text-3xl font-bold">Trending Products 🔥🔥</h2>
           <div className="grid mt-10">
             <div className="grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-3">
-              <ProductCard title="Macbook 16 pro" price="1500000" quantityLeft={50}/>
+              <ProductCard title="Macbook 16 pro" price="1500000" quantityLeft={23}/>
               <ProductCard title="Iphone 16" price="1000000" quantityLeft={50}/>
-              <ProductCard title="Oraimo Pluto Headphone" price="30000" quantityLeft={50}/>
-              <ProductCard title="Macbook 16 pro" price="1200000" quantityLeft={50}/>
+              <ProductCard title="Oraimo Pluto Headphone" price="30000" quantityLeft={100}/>
+              <ProductCard title="Macbook 16 pro" price="1200000" quantityLeft={5}/>
             </div>
           </div>
         </div>
