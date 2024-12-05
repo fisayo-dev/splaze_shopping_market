f79702c648d0db581854f74717d0a759641f174f
fisayo-dev <olufisayobadina@gmail.com>
2024-12-05 11:55:20 +0100

    included import for product card component

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Header from "./Header";
 import Landing from "./Landing";
 import TrendingProducts from "./TrendingProducts";
+import ProductCard from "./ProductCard";
 
-export {Header, Landing, TrendingProducts}
+export {Header, Landing, TrendingProducts, ProductCard}
