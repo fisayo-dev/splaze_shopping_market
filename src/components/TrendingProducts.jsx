3830c08542b70113970ff253c9fb14ddc19a7bea
fisayo-dev <olufisayobadina@gmail.com>
2024-12-05 11:45:22 +0100

    redesigning product card design

--- src/components/TrendingProducts.jsx
+++ src/components/TrendingProducts.jsx
@@ -1,4 +1,5 @@
 import { FaNairaSign } from "react-icons/fa6";
+import landing_social_media from "../assets/app_images/landing_social_media.png";
 
 const TrendingProducts = () => {
   return (
@@ -7,12 +8,16 @@ const TrendingProducts = () => {
         <div className="container mx-auto md:px-16 px-5">
           <h2 className="text-3xl font-bold">Top Purchases 🔥🔥🔥</h2>
           <div className="grid mt-10">
-            <div className="grid grid-cols-4 gap-3">
-              <div className="p-5 rounded-2xl  bg-yellow-200">
-                <img src="" alt="" />
-                <h2 className="text-[1.3rem] font-bold">Oraimo Head Buds</h2>
-                <div className="flex items-center gap-1">
-                  <FaNairaSign />
+            <div className="grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-3">
+              <div className="rounded-2xl hover:scale-105 transition hover:shadow-md  app-gray-color hover-dark-app-gray-color bg-yellow-200">
+                <img src={landing_social_media} alt="" />
+                <div className="py-5 px-4">
+                  <h2 className="text-[1.3rem] font-bold">Oraimo Head Buds</h2>
+                  <div className="flex items-center gap-1">
+                    <FaNairaSign />
+                    <p>35,000.00</p>
+                  </div>
+                  <p className="text-sm">42 items left</p>
                 </div>
               </div>
             </div>
