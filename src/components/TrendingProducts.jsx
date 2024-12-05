c0c388e3c0b6c7f1556013ae0fff4b84e30c3935
fisayo-dev <olufisayobadina@gmail.com>
2024-12-05 16:56:38 +0100

    put dummy product details

--- src/components/TrendingProducts.jsx
+++ src/components/TrendingProducts.jsx
@@ -2,12 +2,15 @@ import ProductCard from "./ProductCard";
 
 const TrendingProducts = () => {
   return (
-    <div className="sticky top-0">
+    <div className="top-0">
       <div className="py-2 bg-white">
         <div className="container mx-auto md:px-16 px-5">
           <h2 className="text-3xl font-bold">Top Purchases 🔥🔥🔥</h2>
           <div className="grid mt-10">
             <div className="grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-3">
+              <ProductCard title="Macbook 16 pro" price="1000" quantityLeft={50}/>
+              <ProductCard title="Iphone 16" price="1000" quantityLeft={50}/>
+              <ProductCard title="Oraimo Pluto Headphone" price="1000" quantityLeft={50}/>
               <ProductCard title="Macbook 16 pro" price="1000" quantityLeft={50}/>
             </div>
           </div>
