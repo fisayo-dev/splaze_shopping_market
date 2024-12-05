b59a91a806b0c05d147b999235ec3758a39440e8
fisayo-dev <olufisayobadina@gmail.com>
2024-12-05 17:23:18 +0100

    removed one of the fire icons

--- src/components/TrendingProducts.jsx
+++ src/components/TrendingProducts.jsx
@@ -5,7 +5,7 @@ const TrendingProducts = () => {
     <div className="top-0">
       <div className="py-2 bg-white">
         <div className="container mx-auto md:px-16 px-5">
-          <h2 className="text-3xl font-bold">Trending Products 🔥🔥🔥</h2>
+          <h2 className="text-3xl font-bold">Trending Products 🔥🔥</h2>
           <div className="grid mt-10">
             <div className="grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-3">
               <ProductCard title="Macbook 16 pro" price="1000" quantityLeft={50}/>
