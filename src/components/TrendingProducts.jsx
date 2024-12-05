dc6f9be5ee40a815ea7a42a54185aadece3e04e7
fisayo-dev <olufisayobadina@gmail.com>
2024-12-05 14:20:40 +0100

    using product card component

--- src/components/TrendingProducts.jsx
+++ src/components/TrendingProducts.jsx
@@ -1,5 +1,6 @@
 import { FaNairaSign } from "react-icons/fa6";
 import landing_social_media from "../assets/app_images/landing_social_media.png";
+import ProductCard from "./ProductCard";
 
 const TrendingProducts = () => {
   return (
@@ -9,17 +10,7 @@ const TrendingProducts = () => {
           <h2 className="text-3xl font-bold">Top Purchases 🔥🔥🔥</h2>
           <div className="grid mt-10">
             <div className="grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-3">
-              <div className="rounded-2xl hover:scale-105 transition hover:shadow-md  app-gray-color hover-dark-app-gray-color bg-yellow-200">
-                <img src={landing_social_media} alt="" />
-                <div className="grid gap-1 py-5 px-4">
-                  <h2 className="text-[1.3rem] font-bold">Oraimo Head Buds</h2>
-                  <div className="flex items-center gap-1">
-                    <FaNairaSign />
-                    <p>35,000.00</p>
-                  </div>
-                  <p className="text-sm">42 items left</p>
-                </div>
-              </div>
+              <ProductCard title="Macbook 16 pro" price="1500000" quantityLeft={50}/>
             </div>
           </div>
         </div>
