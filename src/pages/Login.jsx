5b1dfcfa16f93eae25c1c3ab5ff9817e1d261c1d
fisayo-dev <olufisayobadina@gmail.com>
2024-12-06 22:36:28 +0100

    creating login file

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,70 @@
-const Login = () => {
+import { Global, Lock, User } from "iconsax-react";
+import landing_social_media from "../assets/app_images/landing_social_media.png";
+import { Link } from "react-router-dom";
+import {
+  EnvelopeIcon,
+  KeyIcon,
+  PlusIcon,
+  ShoppingCartIcon,
+  UserGroupIcon,
+} from "@heroicons/react/24/outline";
+import { BsTelephone } from "react-icons/bs";
+import { Button } from "../components";
+import signup_image from "../assets/app_images/460.jpg";
+const Signup = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="md:grid form-grid-login">
+      <div className="md:p-3 p-[1.5rem] bg-yellow-300">
+        <div className="grid h-[100vh] text-center place-content-center gap-4">
+          <div className="flex mx-auto text-black items-center gap-2">
+            <ShoppingCartIcon className="w-12 h-12" />
+            <h2 className="text-3xl font-bold">Splaze</h2>
+          </div>
+          <h2 className="text-xl">I'm glad to have you back </h2>
+          <div className="grid text-[0.97rem] gap-4 md:w-4/5 mx-auto">
+            <div className="grid gap-4 w-full">
+              <div className="rounded-full bg-yellow-100 px-4 py-3">
+                <div className="flex gap-2 items-center">
+                  <EnvelopeIcon className="h-6 w-6" />
+                  <input
+                    type="email"
+                    className="w-full"
+                    placeholder="Email address"
+                  />
+                </div>
+              </div>
+              <div className="rounded-full bg-yellow-100 px-4 py-3">
+                <div className="flex gap-2 items-center">
+                  <KeyIcon className="h-6 w-6" />
+                  <input
+                    type="password"
+                    className="w-full"
+                    placeholder="Password"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
 
-export default Login
+          <p className="text-center">
+            New to Splaze, <br /> Why not{" "}
+            <Link className="font-bold hover:underline" to="/signup">
+              become a customer now
+            </Link>
+          </p>
+
+          <div className="flex mt-5 justify-content-center">
+            <Button styles="mx-auto">Be a customer</Button>
+          </div>
+        </div>
+      </div>
+      <div className="md:grid hidden bg-yellow-50">
+        <div className="grid justify-items-center ">
+          <img src={signup_image} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
