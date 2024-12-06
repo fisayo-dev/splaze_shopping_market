497a7c9c53018c8444fb7e12bc12b95bfca1620e
fisayo-dev <olufisayobadina@gmail.com>
2024-12-06 23:25:47 +0100

    created not found page

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+const NotFound = () => {
+  return (
+    <div className="bg-yellow-300 h-[100vh] w-[100vw]">
+      <div className="grid justify-content-center place-items-center">
+        <h2 className="text-2xl">Page Not Found</h2>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
