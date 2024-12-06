d47bd0bf3b5d1bc1ef09f706e7e54a99c99850c1
fisayo-dev <olufisayobadina@gmail.com>
2024-12-06 16:06:42 +0100

    creating signup page design

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,15 @@
+import landing_social_media from "../assets/app_images/landing_social_media.png";
 const Signup = () => {
   return (
-    <div>Signup</div>
-  )
-}
+    <div className="form-grid-signup">
+      <div className="bg-white">
+        <div className="grid justify-items-center ">
+          <img src={landing_social_media} alt="" />
+        </div>
+      </div>
+      <div className="bg-yellow-300"></div>
+    </div>
+  );
+};
 
-export default Signup
+export default Signup;
