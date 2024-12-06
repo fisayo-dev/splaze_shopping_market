15abf1b9df41850e4636220db2f8dae10d823ddb
fisayo-dev <olufisayobadina@gmail.com>
2024-12-06 16:20:55 +0100

    building signup form

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,4 +1,5 @@
 import landing_social_media from "../assets/app_images/landing_social_media.png";
+import { ShoppingCartIcon } from "@heroicons/react/24/outline";
 const Signup = () => {
   return (
     <div className="form-grid-signup">
@@ -7,7 +8,16 @@ const Signup = () => {
           <img src={landing_social_media} alt="" />
         </div>
       </div>
-      <div className="bg-yellow-300"></div>
+      <div className="bg-yellow-300">
+        <div className="grid h-[100vh] text-center place-content-center justify-items-center gap-4">
+          <div className="flex text-black items-center gap-2">
+            <ShoppingCartIcon className="w-12 h-12" />
+            <h2 className="text-3xl font-bold">Splaze</h2>
+          </div>
+          <h2 className="text-2xl">Register an account with us ?</h2>
+          <div className="grid"></div>
+        </div>
+      </div>
     </div>
   );
 };
