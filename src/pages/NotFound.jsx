946934186afc6f90f92c6326181137723e78b512
fisayo-dev <olufisayobadina@gmail.com>
2024-12-06 23:33:15 +0100

    written into the not found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,10 @@
+import { Logo } from "../components";
+
 const NotFound = () => {
   return (
-    <div className="bg-yellow-300 h-[100vh] w-[100vw]">
-      <div className="grid justify-content-center place-items-center">
-        <h2 className="text-2xl">Page Not Found</h2>
-      </div>
+    <div className="bg-yellow-300 grid h-[100vh] w-[100vw] place-content-center  justify-items-center gap-5">
+      <Logo />
+      <h2 className="text-3xl font-bold">Page Not Found</h2>
     </div>
   );
 };
