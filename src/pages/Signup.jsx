6cd82739ed91e0a794ec9c2527442110ab21bdfb
fisayo-dev <olufisayobadina@gmail.com>
2024-12-06 20:21:32 +0100

    created input field for register form

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,5 +1,12 @@
+import { Global, Lock, User } from "iconsax-react";
 import landing_social_media from "../assets/app_images/landing_social_media.png";
-import { ShoppingCartIcon } from "@heroicons/react/24/outline";
+import {
+  EnvelopeIcon,
+  KeyIcon,
+  ShoppingCartIcon,
+  UserGroupIcon,
+} from "@heroicons/react/24/outline";
+import { BsTelephone } from "react-icons/bs";
 const Signup = () => {
   return (
     <div className="form-grid-signup">
@@ -9,13 +16,84 @@ const Signup = () => {
         </div>
       </div>
       <div className="bg-yellow-300">
-        <div className="grid h-[100vh] text-center place-content-center justify-items-center gap-4">
-          <div className="flex text-black items-center gap-2">
+        <div className="grid h-[100vh] text-center place-content-center gap-4">
+          <div className="flex mx-auto text-black items-center gap-2">
             <ShoppingCartIcon className="w-12 h-12" />
             <h2 className="text-3xl font-bold">Splaze</h2>
           </div>
-          <h2 className="text-2xl">Register an account with us ?</h2>
-          <div className="grid"></div>
+          <h2 className="text-xl">Register an account with us ?</h2>
+          <div className="grid grid-cols-2 gap-2">
+            <div className="rounded-full bg-yellow-100 px-4 py-3">
+              <div className="flex gap-2 items-center">
+                <User className="h-6 w-6" />
+                <input
+                  type="text"
+                  className="w-full"
+                  placeholder="First Name"
+                />
+              </div>
+            </div>
+            <div className="rounded-full bg-yellow-100 px-4 py-3">
+              <div className="flex gap-2 items-center">
+                <UserGroupIcon className="h-6 w-6" />
+                <input type="text" className="w-full" placeholder="Last Name" />
+              </div>
+            </div>
+          </div>
+          <div className="grid gap-4">
+            <div className="rounded-full bg-yellow-100 px-4 py-3">
+              <div className="flex gap-2 items-center">
+                <EnvelopeIcon className="h-6 w-6" />
+                <input
+                  type="email"
+                  className="w-full"
+                  placeholder="Email address"
+                />
+              </div>
+            </div>
+            <div className="rounded-full bg-yellow-100 px-4 py-3">
+              <div className="flex gap-2 items-center">
+                <KeyIcon className="h-6 w-6" />
+                <input type="text" className="w-full" placeholder="Password" />
+              </div>
+            </div>
+            <div className="rounded-full bg-yellow-100 px-4 py-3">
+              <div className="flex gap-2 items-center">
+                <Lock className="h-6 w-6" />
+                <input
+                  type="text"
+                  className="w-full"
+                  placeholder="Repeat Password"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="grid input-field-grid gap-2">
+            <div className="rounded-full bg-yellow-100 px-4 py-3">
+              <div className="flex gap-2 items-center">
+                <BsTelephone className="h-6 w-6" />
+                <input
+                  type="number"
+                  className="w-full"
+                  placeholder="Phone Number"
+                />
+              </div>
+            </div>
+            <div className="rounded-full bg-yellow-100 px-4 py-3">
+              <div className="flex gap-2 items-center">
+                <Global className="h-6 w-6" />
+                <select type="text" className="w-full" placeholder="Last Name">
+                  <option>Nigeria</option>
+                  <option>Ghana</option>
+                  <option>United States</option>
+                  <option>United Kingdom</option>
+                  <option>Russia</option>
+                  <option>China</option>
+                  <option>Japan</option>
+                </select>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
