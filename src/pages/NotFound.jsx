a44a032dd0d0a852a8f12f5a4cfcff8a86e9a144
fisayo-dev <olufisayobadina@gmail.com>
2024-12-06 23:54:28 +0100

    put some content in the not found file

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,10 +1,9 @@
-import { Logo } from "../components";
-
 const NotFound = () => {
   return (
-    <div className="bg-yellow-300 grid h-[100vh] w-[100vw] place-content-center  justify-items-center gap-5">
-      <Logo />
-      <h2 className="text-3xl font-bold">Page Not Found</h2>
+    <div className="bg-yellow-300">
+      <div className="grid h-[100vh] w-[100vw] items-center justify-items-center">
+        <h2 className="text-3xl font-bold">Page Not Found</h2>
+      </div>
     </div>
   );
 };
