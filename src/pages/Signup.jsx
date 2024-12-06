e7d31f43d5142e08bbc4daa1755d7a2303aec408
fisayo-dev <olufisayobadina@gmail.com>
2024-12-06 20:29:41 +0100

    making the last section of inputs

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -3,6 +3,7 @@ import landing_social_media from "../assets/app_images/landing_social_media.png"
 import {
   EnvelopeIcon,
   KeyIcon,
+  PlusIcon,
   ShoppingCartIcon,
   UserGroupIcon,
 } from "@heroicons/react/24/outline";
@@ -68,7 +69,7 @@ const Signup = () => {
               </div>
             </div>
           </div>
-          <div className="grid input-field-grid gap-2">
+          <div className="grid country-phone-input-grid gap-2">
             <div className="rounded-full bg-yellow-100 px-4 py-3">
               <div className="flex gap-2 items-center">
                 <BsTelephone className="h-6 w-6" />
@@ -78,6 +79,18 @@ const Signup = () => {
                   placeholder="Phone Number"
                 />
               </div>
+            </div>
+            <div className="flex px-4 py-3 rounded-full bg-yellow-100 items-center">
+              <PlusIcon className="h-6 w-6" />
+              <select type="text" placeholder="Last Name">
+                <option>234</option>
+                <option>234</option>
+                <option>166</option>
+                <option>134</option>
+                <option>135</option>
+                <option>248</option>
+                <option>301</option>
+              </select>
             </div>
             <div className="rounded-full bg-yellow-100 px-4 py-3">
               <div className="flex gap-2 items-center">
