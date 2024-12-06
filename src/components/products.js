f2c96b2e6dbbe7b63813435451af3d91519b4175
fisayo-dev <olufisayobadina@gmail.com>
2024-12-06 13:07:25 +0100

    create dummy products file

---
+++ src/components/products.js
@@ -0,0 +1,38 @@
+const product = [
+    {
+        title: "Asus Zenbook OLed Fold",
+        description: "Lorem ispum dor lor sit amet conseputeur",
+        price: "5000000",
+        quantity: 12, 
+    },
+    {
+        title: "Goldern Morn" ,
+        description: "Lorem ispum dor lor sit amet conseputeur",
+        price: "5000",
+        quantity: 34, 
+    },
+    {
+        title: "Ice cream - vanilla flavour",
+        description: "Lorem ispum dor lor sit amet conseputeur",
+        price: "8000",
+        quantity: 5, 
+    },
+    {
+        title: "Father's mug",
+        description: "Lorem ispum dor lor sit amet conseputeur",
+        price: "6000",
+        quantity: 200, 
+    },
+    {
+        title: "Apple Laptop Bag ",
+        description: "Lorem ispum dor lor sit amet conseputeur",
+        price: "12,000",
+        quantity: 19, 
+    },
+    {
+        title: "Zinx AC Power Bank",
+        description: "Lorem ispum dor lor sit amet conseputeur",
+        price: "670000",
+        quantity: 500, 
+    },
+]
