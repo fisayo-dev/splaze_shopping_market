a19c9f26e9345805372cf1f87338a61c5d5a8bac
fisayo-dev <olufisayobadina@gmail.com>
2024-12-07 22:27:13 +0100

    setting out page height

--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -2,9 +2,9 @@ import { StoreSideBar } from "../components";
 
 const Store = () => {
   return (
-    <div className="store-sidebar-grid">
+    <div className="store-sidebar-grid h-[100vh] overflow-hidden">
       <StoreSideBar />
-      <div className="py-5">
+      <div className="py-5 overflow-scroll">
         <div className="grid"></div>
       </div>
     </div>
