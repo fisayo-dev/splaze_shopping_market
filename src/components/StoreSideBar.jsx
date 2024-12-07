1849eb78609c0071bfb8e4c0ce56da6e1169164d
fisayo-dev <olufisayobadina@gmail.com>
2024-12-07 22:10:49 +0100

    added categories to store

--- src/components/StoreSideBar.jsx
+++ src/components/StoreSideBar.jsx
@@ -1,9 +1,61 @@
+import { Copyright, Electricity } from "iconsax-react";
 import Logo from "./Logo";
+import { PiDress } from "react-icons/pi";
 const StoreSideBar = () => {
   return (
     <div className="bg-yellow-300 h-[100vh]">
       <div className="p-8">
-        <Logo />
+        <div className="grid gap-10">
+          <Logo />
+          <div className="grid gap-2">
+            <div className="bg-yellow-200 rounded-xl px-4 py-3">
+              <div className="flex items-center gap-2">
+                <Electricity className="h-6 w-6" />
+                <p>Electronics</p>
+              </div>
+            </div>
+            <div className="bg-yellow-200 rounded-xl px-4 py-3">
+              <div className="flex items-center gap-2">
+                <PiDress className="h-6 w-6" />
+                <p>Fashion</p>
+              </div>
+            </div>
+            <div className="bg-yellow-200 rounded-xl px-4 py-3">
+              <div className="flex items-center gap-2">
+                <Electricity className="h-6 w-6" />
+                <p>Home and Kitchen</p>
+              </div>
+            </div>
+            <div className="bg-yellow-200 rounded-xl px-4 py-3">
+              <div className="flex items-center gap-2">
+                <Electricity className="h-6 w-6" />
+                <p>Health and Beauty</p>
+              </div>
+            </div>
+            <div className="bg-yellow-200 rounded-xl px-4 py-3">
+              <div className="flex items-center gap-2">
+                <Electricity className="h-6 w-6" />
+                <p>Sports and Outdoors</p>
+              </div>
+            </div>
+            <div className="bg-yellow-200 rounded-xl px-4 py-3">
+              <div className="flex items-center gap-2">
+                <Electricity className="h-6 w-6" />
+                <p>Books</p>
+              </div>
+            </div>
+            <div className="bg-yellow-200 rounded-xl px-4 py-3">
+              <div className="flex items-center gap-2">
+                <Electricity className="h-6 w-6" />
+                <p>Gadgets and Devices</p>
+              </div>
+            </div>
+          </div>
+          <div className="flex items-center gap-1">
+            <Copyright className="h-6 w-6" />
+            <p>Copyright 2024</p>
+          </div>
+        </div>
       </div>
     </div>
   );
