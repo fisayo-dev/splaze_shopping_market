e70574577c46b8d11469bf4f45a703e61869ccb9
fisayo-dev <olufisayobadina@gmail.com>
2024-12-07 16:07:22 +0100

    style: changed the style of trending header font style

--- src/components/TrendingProducts.jsx
+++ src/components/TrendingProducts.jsx
@@ -6,7 +6,7 @@ const TrendingProducts = () => {
     <div className="top-0">
       <div className="py-2 bg-white">
         <div className="container mx-auto 2xl:px-16 md:px-10  px-5">
-          <h2 className="text-3xl font-bold">Trending Products 🔥🔥</h2>
+          <h2 className="md:text-3xl text-[1.5rem] font-bold">Trending Products 🔥🔥</h2>
           <div className="grid mt-10">
             <div className="grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-3">
               {products.map((product, index) => (
