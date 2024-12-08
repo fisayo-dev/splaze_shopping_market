bd8808e451913eebf3daf07d5a7bc455fb70e4b1
fisayo-dev <olufisayobadina@gmail.com>
2024-12-08 13:03:37 +0100

    gave all category btn a cursor pointer

--- src/components/StoreSideBar.jsx
+++ src/components/StoreSideBar.jsx
@@ -11,49 +11,49 @@ const StoreSideBar = () => {
         <div className="grid gap-10">
           <Logo />
           <div className="grid gap-2">
-            <div className="bg-white shadow-sm rounded-xl px-4 py-3">
+            <div className="bg-white shadow-sm rounded-xl px-4 cursor-pointer py-3">
               <div className="flex items-center gap-2">
                 <ViewColumnsIcon className="h-6 w-6" />
                 <p>All</p>
               </div>
             </div>
-            <div className="bg-yellow-200 hover:bg-white hover:bg-opacity-65 rounded-xl px-4 py-3">
+            <div className="bg-yellow-200 hover:bg-white hover:bg-opacity-65 cursor-pointer rounded-xl px-4 py-3">
               <div className="flex items-center gap-2">
                 <Electricity className="h-6 w-6" />
                 <p>Electronics</p>
               </div>
             </div>
-            <div className="bg-yellow-200 hover:bg-white hover:bg-opacity-65 rounded-xl px-4 py-3">
+            <div className="bg-yellow-200 hover:bg-white hover:bg-opacity-65 cursor-pointer rounded-xl px-4 py-3">
               <div className="flex items-center gap-2">
                 <PiDress className="h-6 w-6" />
                 <p>Fashion</p>
               </div>
             </div>
-            <div className="bg-yellow-200 hover:bg-white hover:bg-opacity-65 rounded-xl px-4 py-3">
+            <div className="bg-yellow-200 hover:bg-white hover:bg-opacity-65 cursor-pointer rounded-xl px-4 py-3">
               <div className="flex items-center gap-2">
                 <BuildingStorefrontIcon className="h-6 w-6" />
                 <p>Home and Kitchen</p>
               </div>
             </div>
-            <div className="bg-yellow-200 hover:bg-white hover:bg-opacity-65 rounded-xl px-4 py-3">
+            <div className="bg-yellow-200 hover:bg-white hover:bg-opacity-65 cursor-pointer rounded-xl px-4 py-3">
               <div className="flex items-center gap-2">
                 <Heart className="h-6 w-6" />
                 <p>Health and Beauty</p>
               </div>
             </div>
-            <div className="bg-yellow-200 hover:bg-white hover:bg-opacity-65 rounded-xl px-4 py-3">
+            <div className="bg-yellow-200 hover:bg-white hover:bg-opacity-65 cursor-pointer rounded-xl px-4 py-3">
               <div className="flex items-center gap-2">
                 <CiFootball className="h-6 w-6" />
                 <p>Sports and Outdoors</p>
               </div>
             </div>
-            <div className="bg-yellow-200 hover:bg-white hover:bg-opacity-65 rounded-xl px-4 py-3">
+            <div className="bg-yellow-200 hover:bg-white hover:bg-opacity-65 cursor-pointer rounded-xl px-4 py-3">
               <div className="flex items-center gap-2">
                 <Book className="h-6 w-6" />
                 <p>Books</p>
               </div>
             </div>
-            <div className="bg-yellow-200 hover:bg-white hover:bg-opacity-65 rounded-xl px-4 py-3">
+            <div className="bg-yellow-200 hover:bg-white hover:bg-opacity-65 cursor-pointer rounded-xl px-4 py-3">
               <div className="flex items-center gap-2">
                 <FaLaptop className="h-6 w-6" />
                 <p>Gadgets and Devices</p>
